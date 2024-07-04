c329fc2db22caa4cb032439a2f7f8443bb94bfd2
Anthony Harper <gaiansentience@gmail.com>
2024-07-04 14:02:46 -0600

    move initial drafts to archived

--- scripts/examples/drop.all_user_objects.sql
+++ scripts/examples/drop.all_user_objects.sql
@@ -1,4 +1,4 @@
-prompt dropping all user objects
+prompt dropping all example objects for PRACTICALPLSQL
 set serveroutput on;
 
 declare
@@ -8,9 +8,13 @@ object_type || ' ' || object_name as object,
 'drop ' || object_type || ' ' || object_name 
 || case object_type when 'TABLE' then ' purge' end as ddl
 from user_objects
-where object_type in ('FUNCTION','PROCEDURE','TYPE','TABLE','VIEW','SEQUENCE');
+where object_type in ('FUNCTION','PROCEDURE','TYPE','TABLE','VIEW','SEQUENCE', 'PACKAGE');
 
 begin
+
+    if user not in ('CLOUD_PRACTICALPLSQL', 'PRACTICALPLSQL') then
+        raise_application_error(-20100, 'script intended to remove all example objects in PRACTICALPLSQL schema, cancelling execution for ' || user);
+    end if;
 
     for r in c loop
         dbms_output.put_line('dropping ' || r.object);
