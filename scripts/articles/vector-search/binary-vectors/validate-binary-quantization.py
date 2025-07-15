8a53da484ea1c5415358eaca43909d1be2739498
Anthony Harper <gaiansentience@gmail.com>
2025-07-15 19:17:46 -0400

    Python validations for quantize macro output

---
+++ scripts/articles/vector-search/binary-vectors/validate-binary-quantization.py
@@ -0,0 +1,56 @@
+#
+# vector quantized with macro in database
+# dimensions: 8 format: FLOAT32
+# [-1.23000003E-001,-6.53999984E-001,3.44999999E-001,-1.99999996E-002,7.88999975E-001,-5.66999972E-001,8.88000011E-001,0]
+# dimensions: 8 format: BINARY
+# [42]
+# quantize with sentence transformers:
+# import sentence_transformers
+from sentence_transformers import SentenceTransformer
+from sentence_transformers.quantization import quantize_embeddings
+embeddings = [[-1.23000003E-001,-6.53999984E-001,3.44999999E-001,-1.99999996E-002,7.88999975E-001,-5.66999972E-001,8.88000011E-001,0]]
+embeddings_binary = quantize_embeddings(embeddings, precision="ubinary")
+print(embeddings_binary)
+#
+#
+# vector quantized with macro in database
+# dimensions: 16 format: FLOAT32
+# [-1.23000003E-001,-6.53999984E-001,3.44999999E-001,-1.99999996E-002,7.88999975E-001,-5.66999972E-001,8.88000011E-001,0,-1.23000003E-001,-4.44000006E-001,-6.49999976E-001,-1.99999996E-002,2.31000006E-001,-1.09999999E-001,7.30000019E-001,2.19999999E-001]
+# dimensions: 16 format: BINARY
+# [42,11]
+# quantize with sentence transformers:
+# import sentence_transformers
+from sentence_transformers import SentenceTransformer
+from sentence_transformers.quantization import quantize_embeddings
+embeddings = [[-1.23000003E-001,-6.53999984E-001,3.44999999E-001,-1.99999996E-002,7.88999975E-001,-5.66999972E-001,8.88000011E-001,0,-1.23000003E-001,-4.44000006E-001,-6.49999976E-001,-1.99999996E-002,2.31000006E-001,-1.09999999E-001,7.30000019E-001,2.19999999E-001]]
+embeddings_binary = quantize_embeddings(embeddings, precision="ubinary")
+print(embeddings_binary)
+#
+#
+# vector quantized with macro in database
+# dimensions: 24 format: FLOAT32
+# [-1.23000003E-001,-6.53999984E-001,3.44999999E-001,-1.99999996E-002,7.88999975E-001,-5.66999972E-001,8.88000011E-001,0,-1.23000003E-001,-4.44000006E-001,-6.49999976E-001,-1.99999996E-002,2.31000006E-001,-1.09999999E-001,7.30000019E-001,0,1.19999997E-001,3.30000013E-001,5.60000002E-001,-7.69999981E-001,8.79999995E-001,-9.9000001E-001,2.09999993E-001,4.19999987E-001]
+# dimensions: 24 format: BINARY
+# [42,10,235]
+# quantize with sentence transformers:
+# import sentence_transformers
+from sentence_transformers import SentenceTransformer
+from sentence_transformers.quantization import quantize_embeddings
+embeddings = [[-1.23000003E-001,-6.53999984E-001,3.44999999E-001,-1.99999996E-002,7.88999975E-001,-5.66999972E-001,8.88000011E-001,0,-1.23000003E-001,-4.44000006E-001,-6.49999976E-001,-1.99999996E-002,2.31000006E-001,-1.09999999E-001,7.30000019E-001,0,1.19999997E-001,3.30000013E-001,5.60000002E-001,-7.69999981E-001,8.79999995E-001,-9.9000001E-001,2.09999993E-001,4.19999987E-001]]
+embeddings_binary = quantize_embeddings(embeddings, precision="ubinary")
+print(embeddings_binary)
+#
+#
+# vector quantized with macro in database
+# dimensions: 32 format: FLOAT32
+# [-1.23000003E-001,-6.53999984E-001,3.44999999E-001,-1.99999996E-002,7.88999975E-001,-5.66999972E-001,8.88000011E-001,0,-1.23000003E-001,-4.44000006E-001,-6.49999976E-001,-1.99999996E-002,2.31000006E-001,-1.09999999E-001,7.30000019E-001,0,1.19999997E-001,-3.30000013E-001,5.60000002E-001,7.69999981E-001,8.79999995E-001,-9.9000001E-001,2.09999993E-001,-4.19999987E-001,-5.0999999E-001,-6.10000014E-001,7.09999979E-001,8.10000002E-001,-9.10000026E-001,-1.00999999E+000,1.29999995E-001,2.30000004E-001]
+# dimensions: 32 format: BINARY
+# [42,10,186,51]
+# quantize with sentence transformers:
+# import sentence_transformers
+from sentence_transformers import SentenceTransformer
+from sentence_transformers.quantization import quantize_embeddings
+embeddings = [[-1.23000003E-001,-6.53999984E-001,3.44999999E-001,-1.99999996E-002,7.88999975E-001,-5.66999972E-001,8.88000011E-001,0,-1.23000003E-001,-4.44000006E-001,-6.49999976E-001,-1.99999996E-002,2.31000006E-001,-1.09999999E-001,7.30000019E-001,0,1.19999997E-001,-3.30000013E-001,5.60000002E-001,7.69999981E-001,8.79999995E-001,-9.9000001E-001,2.09999993E-001,-4.19999987E-001,-5.0999999E-001,-6.10000014E-001,7.09999979E-001,8.10000002E-001,-9.10000026E-001,-1.00999999E+000,1.29999995E-001,2.30000004E-001]]
+embeddings_binary = quantize_embeddings(embeddings, precision="ubinary")
+print(embeddings_binary)
+#
