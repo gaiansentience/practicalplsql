402c7003d07fb71541fc3a922aa697f5a063fef9
Anthony Harper <gaiansentience@gmail.com>
2025-08-10 15:50:26 -0400

    Updated title code sample with reranking search

--- scripts/articles/vector-search/binary-vector-search/title_code_sample.sql
+++ scripts/articles/vector-search/binary-vector-search/title_code_sample.sql
@@ -1,57 +1,66 @@
 --title_code_sample.sql
-
 set serveroutput on;
-
 declare
-    v_float64 vector(*, float64);
-    v_float32 vector(*, float32);
-    v_int8    vector(*, int8);
-    v_binary  vector(*, binary);    
+    l_search_text varchar2(100);
+    l_search_float  vector(*, float32);
+    l_search_binary vector(*, binary);
+    l_results sys.odcivarchar2list;
 begin
-    v_float64 := to_vector(
-        '[
-           -6.28318530717958647692,-1.41421356237309504880,
-            3.14159265358979323846,-1.61803398874989484820,
-            2.71828182845904523536,-0.56714329040978387299,
-            2.39996322972865332223,-0.83462684167407318628
-        ]'
-        , *, float64);   
-    dbms_output.put_line(vector_serialize(v_float64));
+    l_search_text := 'A wickedly delectable, delicious and decadent dessert.';
+
+    select
+        vector_embedding(
+            ALL_MINILM_L12_V2 
+            using l_search_text as data)
+        , to_binary_vector(
+            vector_embedding(
+                MXBAI_EMBED_LARGE_V1 
+                using l_search_text as data)
+            )
+        into l_search_float, l_search_binary;
+
+    with fast_binary_search as (
+        select 
+            i.item_name
+            , v.embedding3 as embedding_float
+        from 
+            menu_items i
+            join menu_vectors v on i.item_id = v.item_id
+        order by 
+            vector_distance(
+                v.embedding1_binary
+                , l_search_binary
+                , hamming)
+        fetch first 25 rows only   
+    ), precise_float_search as (
+        select item_name
+        from fast_binary_search b
+        order by 
+            vector_distance(
+                b.embedding_float
+                , l_search_float
+                , cosine)
+        fetch first 5 rows only
+    )
+    select item_name
+    bulk collect into l_results
+    from precise_float_search;
     
-    select vector(v_float64, 8, float32) into v_float32;
-    dbms_output.put_line(vector_serialize(v_float32));
-    
-    select vector(v_float32, 8, int8) into v_int8;
-    dbms_output.put_line(vector_serialize(v_int8));
-    
-    select to_vector(v_int8, 8, binary) into v_binary;
-        
-exception
-    when others then
-        dbms_output.put_line(sqlerrm);
+    for v in values of l_results loop
+        dbms_output.put_line(v);
+    end loop;
+
 end;
 /
 
+/*
 
-/* SCRIPT OUTPUT:
-
-[
--6.2831853071795862E+000,-1.4142135623730951E+000,
-3.1415926535897931E+000,-1.6180339887498949E+000,
-2.7182818284590451E+000,-5.6714329040978384E-001,
-2.3999632297286535E+000,-8.3462684167407319E-001
-]
-
-[
--6.28318548E+000,-1.41421354E+000,
-3.14159274E+000,-1.61803401E+000,
-2.71828175E+000,-5.67143261E-001,
-2.39996314E+000,-8.34626853E-001
-]
-
-[-6,-1,3,-2,3,-1,2,-1]
-
-ORA-51814: Vector of BINARY format cannot have any operation performed with vector of any other type.
+Turtle Cheesecake
+Chocolate Soufflé
+Chocolate Mousse
+Peach Cobbler
+Molten Lava Cake
 
 */
+
 
