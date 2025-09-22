3aa1ad4c73ece39727909be424ae96f655c67861
Anthony Harper <gaiansentience@gmail.com>
2025-09-22 06:59:49 -0400

    in progress

---
+++ scripts/articles/vector-search/binary-vector-indexes/create.vector_indexes.jaccard.sql
@@ -0,0 +1,17 @@
+
+
+drop index if exists menu_vectors_eb0;
+
+create vector index menu_vectors_eb0 on menu_vectors (embedding_binary) 
+organization neighbor partitions
+distance jaccard
+with target accuracy 80;
+
+drop index if exists menu_vectors_eb1;
+
+create vector index menu_vectors_eb1 on menu_vectors (embedding1_binary) 
+organization neighbor partitions
+distance jaccard
+with target accuracy 80;
+
+
