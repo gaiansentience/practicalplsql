07ae8e355f0dd1aed865b4825f6ede720d893a9a
Anthony Harper <gaiansentience@gmail.com>
2026-03-15 21:13:09 -0400

    archive working code for example 3 prior to making changes

---
+++ scripts/articles/sql-assertions/effective-dated-assertions-3/archived/example-3-03-view.sql
@@ -0,0 +1,22 @@
+create or replace view review_order_discounts as
+select 
+    c.customer_name, p.status, (100 * s.discount_min) || '%' as loyalty_discount
+    , o.order_id
+    , (100 * o.discount) || '%' as order_discount
+    , case 
+        when o.discount < s.discount_min then 'Insufficient' 
+        when o.discount = s.discount_min then 'Meets Minimum' 
+        else 'Exceeds Minimum' 
+    end as discount_valid
+    , o.placed
+    , p.effective
+    , p.expires
+from 
+    customers c 
+    join customer_loyalty p on c.customer_name = p.customer_name
+    join loyalty s on p.status = s.status
+    join orders o on c.customer_name = o.customer_name 
+where
+    o.placed >= p.effective and (p.expires is null or o.placed < p.expires)
+order by c.customer_name, o.order_id
+/
